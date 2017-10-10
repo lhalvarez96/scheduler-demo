7022f1ae7f3066fb9a62d795fd2ed2fecb709db1
lhalvarez96 <lhalvarezrivera@gmail.com>
2017-10-10 00:32:17 -0300

    Temporizadores

--- src/main.c
+++ src/main.c
@@ -2,12 +2,28 @@
 
 int main() {
   int a[5];
-  int n;
+  int n,j,k;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
   for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+	k=1;
+
+	for(j=0; j<=4; j++){
+		
+		if((i+1)%a[j]==0){
+			printf("%d",j);
+			k=0;
+		}
+		
+	}
+
+if(k==1){
+printf("-");
+
+}			
+
+printf("\n");
   }
 
   return 0;
